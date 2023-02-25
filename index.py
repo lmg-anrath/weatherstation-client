2934f6dcc238fa935d8721f98952f528c0b4fc74
JH220 <admin@jh220.de>
2023-02-25 13:24:13 +0100

    Added data logging when offline and Error catching if sensors are down

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import json
 import requests
 import time
@@ -9,46 +10,104 @@ import Adafruit_DHT
 import bme280
 
 config = json.load(open('config.json'))
-# log = json.load(open('data.json'))
 timestamp = str(round(time.time()))
 
 print('Reading data...')
+errors = {}
 
-humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
-temperature = str(round(temperature, 2))
-humidity = str(round(humidity, 0))
+temperature = None
+humidity = None
+try:
+	humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.AM2302, 17)
+	temperature = str(round(temperature, 2))
+	humidity = str(round(humidity, 0))
+except Exception as e:
+	print('Error while reading data from AM2302!')
+	print(e)
+	errors['AM2302'] = str(e);
 
-temperature2, air_pressure, humidity2 = bme280.readBME280All()
-air_pressure = str(round(air_pressure,0))
+temperature2 = None
+air_pressure = None
+humidity2 = None
+try:
+	temperature2, air_pressure, humidity2 = bme280.readBME280All()
+	temperature2 = str(round(temperature2, 2))
+	humidity = str(round(humidity, 0))
+	air_pressure = str(round(air_pressure, 0))
+except Exception as e:
+	print('Error while reading data from BME280!')
+	print(e)
+	errors['BME280'] = str(e);
 
-air_particle = SDS011("/dev/ttyUSB0", use_query_mode=True).query()
-air_particle_pm25 = str(air_particle[0])
-air_particle_pm10 = str(air_particle[1])
+if temperature == None and temperature2 != None:
+	temperature = temperature2
+if humidity == None and humidity2 != None:
+	humidity = humidity2
 
-print(f'Temp: {temperature} °C, Humidity: {humidity} %, Air pressure: {air_pressure} hPa, Air particle: {air_particle_pm25} pm25 and {air_particle_pm10} pm10')
+air_particle_pm25 = None
+air_particle_pm10 = None
+try:
+	air_particle = SDS011("/dev/ttyUSB0", use_query_mode=True).query()
+	air_particle_pm25 = str(air_particle[0])
+	air_particle_pm10 = str(air_particle[1])
+except Exception as e:
+	print('Error while reading data from SDS011!')
+	print(e)
+	errors['SDS011'] = str(e);
+
+print(f'Temp: {temperature} °C & {temperature2} °C, Humidity: {humidity} & {humidity2} %, Air pressure: {air_pressure} hPa, Air particle: {air_particle_pm25} pm25 and {air_particle_pm10} pm10')
 print('The data has been collected.')
 
-data = {
-	'timestamp': timestamp,
-	'temperature': temperature,
-	'humidity': humidity,
-	'air_pressure': air_pressure,
-	'air_particle_pm25': air_particle_pm25,
-	'air_particle_pm10': air_particle_pm10
-}
-
-# print('Logging data in \'data.json\'')
-# log.append(data)
-# json.dump(log, open('data.json', 'w'), indent = 4, separators = (',',': '))
-# print('Data successfully saved!')
-
+data = { 'timestamp': timestamp }
+if temperature != None: data['temperature'] = temperature
+if humidity != None: data['humidity'] = humidity
+if air_pressure != None: data['air_pressure'] = air_pressure
+if air_particle_pm25 != None: data['air_particle_pm25'] = air_particle_pm25
+if air_particle_pm10 != None: data['air_particle_pm10'] = air_particle_pm10
 
 print('Uploading data to the server...');
+online = False
 
 try:
 	res = requests.post(config['url'] + '/v2/stations/' + str(config['stationId'] - 1), data=data, headers={ 'Authorization': config['accessToken'] })
 
 	print('Upload completed with status code ' + str(res.status_code) + '!')
 	print('Response from server: ' + res.text)
+	online = True
 except requests.ConnectionError:
 	print('Error while upload data to the server! Check your internet connection.')
+
+	print('Temporary logging data in \'data.json\'')
+	if not os.path.isfile('data.json'):
+		json.dump([], open('data.json', 'w'))
+	data_list = json.load(open('data.json'))
+	data_list.append(data)
+	json.dump(data_list, open('data.json', 'w'))
+	print('Data has been saved in \'data.json\'')
+
+if len(errors) > 0 and online:
+	print('Uploading errors to the server...')
+	try:
+		res = requests.post(config['url'] + '/v2/stations/' + str(config['stationId'] - 1) + '/error', data=errors, headers={ 'Authorization': config['accessToken'] })
+		print('Upload completed with status code ' + str(res.status_code) + '!')
+		print('Response from server: ' + res.text)
+	except requests.ConnectionError:
+		print('Error while upload errors to the server! Check your internet connection.')
+
+if os.path.isfile('data.json') and online:
+	print('Uploading previous data to the server...')
+	data_list = json.load(open('data.json'))
+	if len(data_list) == 0:
+		print('No data to upload.')
+		os.remove('data.json')
+		exit()
+
+	try:
+		res = requests.post(config['url'] + '/v2/stations/' + str(config['stationId'] - 1) + '/bulk', data=data_list, headers={ 'Authorization': config['accessToken'] })
+		print('Upload completed with status code ' + str(res.status_code) + '!')
+		print('Response from server: ' + res.text)
+		if (res.status_code == 200):
+			os.remove('data.json')
+			print('\'data.json\' has been removed.')
+	except requests.ConnectionError:
+		print('Error while upload previous data to the server! Check your internet connection.')
